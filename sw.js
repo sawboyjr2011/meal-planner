03ae67e5b993bdb06d37a5c36e03c66d6fbcc16e
sawboyjr2011 <fmoscaljr@gmail.com>
2026-03-01 18:20:06 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE = "mp-shell-v3";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cached) => cached || fetch(evt.request))
+  );
+});
