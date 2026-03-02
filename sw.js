68610019cddce11b829869a8b0e1be17d78291f9
sawboyjr2011 <fmoscaljr@gmail.com>
2026-03-01 20:55:16 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mp-shell-v3";
+const CACHE = "mp-shell-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (evt) => {
